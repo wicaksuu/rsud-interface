c4ee61194770eab4b50180f68d7939a07137269e
wicaksuu <wicaksono.bayuaji.wba@gmail.com>
2024-03-13 22:48:20 +0700

    add auto role user

---
+++ app/Livewire/Pendaftaran/LandingPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pendaftaran;
+
+use Livewire\Component;
+
+class LandingPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.pendaftaran.landing-page');
+    }
+}
